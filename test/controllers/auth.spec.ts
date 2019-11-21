3e859306e988e54c438e8fc1058728fc85d2dabb
Filip Egeric <filip.egeric@gmail.com>
2019-11-21 22:14:46 +0100

    Adds a number of tests

--- test/controllers/auth.spec.ts
+++ test/controllers/auth.spec.ts
@@ -1,7 +1,12 @@
+import { hash } from 'argon2';
+import { assert } from 'chai';
+import { internet, name, random } from 'faker';
 import { Redis } from 'ioredis';
 import { Transporter } from 'nodemailer';
+import sinon from 'sinon';
 import { Repository } from 'typeorm';
 
+import { config } from '../../src/config';
 import { AuthController } from '../../src/controllers/auth.controller';
 import { User } from '../../src/entity/user.entity';
 import { AuthService } from '../../src/services/auth.service';
@@ -20,64 +25,264 @@ describe('AuthController', () => {
     emailService
   );
   const controller = new AuthController(userService, authService);
-  // ! Preventing compiler from crashing because of unused variable
-  // tslint:disable-next-line: no-unused-expression
-  controller;
 
   describe('register', () => {
-    it('creates a new user', () => {
-      throw new Error('Not implemented yet');
+    it('creates a new user', async () => {
+      const stub = sinon.stub(userService, 'createUser');
+      await controller.register({
+        body: {
+          email: internet.email(),
+          password: internet.password(),
+          fullName: name.findName()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+      assert(stub.called);
+      stub.restore();
     });
   });
 
   describe('login', () => {
-    it('calls login on user service', () => {
-      throw new Error('Not implemented yet');
+    it('calls login on auth service', async () => {
+      const stub = sinon
+        .stub(authService, 'login')
+        .returns(Promise.resolve({ accessToken: '', refreshToken: '' }));
+      await controller.login({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+      assert(stub.called);
+      stub.restore();
     });
-    it('returns http response with cookie for refresh token', () => {
-      throw new Error('Not implemented yet');
+    it('returns http response with cookie for refresh token', async () => {
+      const refreshToken = random.alphaNumeric();
+      const stub = sinon
+        .stub(authService, 'login')
+        .returns(Promise.resolve({ accessToken: '', refreshToken }));
+      const response = await controller.login({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+      assert(
+        response.cookies &&
+          response.cookies.some(
+            el =>
+              el.key === config.JWT.REFRESH_TOKEN_COOKIE_KEY &&
+              el.value === refreshToken
+          )
+      );
+      stub.restore();
     });
-    it('returns http response with access token in data', () => {
-      throw new Error('Not implemented yet');
+    it('returns http response with access token in data', async () => {
+      const accessToken = random.alphaNumeric();
+      const stub = sinon
+        .stub(authService, 'login')
+        .returns(Promise.resolve({ accessToken, refreshToken: '' }));
+      const response = await controller.login({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+      assert(response.data && response.data.accessToken === accessToken);
+      stub.restore();
     });
   });
 
   describe('refresh', () => {
-    it("throws if cookie isn't present in request", () => {
-      throw new Error('Not implemented yet');
+    it("throws if cookie isn't present in request", async () => {
+      const stub = sinon.stub(authService, 'refresh');
+      try {
+        await controller.refresh({
+          body: {},
+          cookies: {},
+          ip: internet.ip(),
+          params: {},
+          query: {}
+        });
+        assert(false, "It didn't throw an exception");
+      } catch (error) {
+        assert(error);
+      }
+      stub.restore();
     });
-    it('calls refresh on auth service', () => {
-      throw new Error('Not implemented yet');
+    it('calls refresh on auth service', async () => {
+      const stub = sinon
+        .stub(authService, 'refresh')
+        .returns(Promise.resolve({ accessToken: '', refreshToken: '' }));
+
+      await controller.refresh({
+        body: {},
+        cookies: {
+          [config.JWT.REFRESH_TOKEN_COOKIE_KEY]: random.alphaNumeric()
+        },
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+
+      assert(stub.called);
+
+      stub.restore();
     });
-    it('returns http response with cookie for refresh token', () => {
-      throw new Error('Not implemented yet');
+    it('returns http response with cookie for refresh token', async () => {
+      const refreshToken = random.alphaNumeric();
+      const stub = sinon
+        .stub(authService, 'refresh')
+        .returns(Promise.resolve({ accessToken: '', refreshToken }));
+      userRepository.findOne = sinon.stub().returns(
+        Promise.resolve({
+          id: random.uuid(),
+          fullName: name.findName(),
+          email: internet.email(),
+          password: internet.password(),
+          tokenVersion: 0,
+          isGoogleUser: false
+        })
+      );
+      const response = await controller.refresh({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {
+          [config.JWT.REFRESH_TOKEN_COOKIE_KEY]: random.alphaNumeric()
+        },
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+      assert(
+        response.cookies &&
+          response.cookies.some(
+            el =>
+              el.key === config.JWT.REFRESH_TOKEN_COOKIE_KEY &&
+              el.value === refreshToken
+          )
+      );
+      stub.restore();
     });
-    it('returns http response with access token in data', () => {
-      throw new Error('Not implemented yet');
+    it('returns http response with access token in data', async () => {
+      const accessToken = random.alphaNumeric();
+      const stub = sinon
+        .stub(authService, 'refresh')
+        .returns(Promise.resolve({ accessToken, refreshToken: '' }));
+      userRepository.findOne = sinon.stub().returns(
+        Promise.resolve({
+          id: random.uuid(),
+          fullName: name.findName(),
+          email: internet.email(),
+          password: await hash(internet.password()),
+          tokenVersion: 0,
+          isGoogleUser: false
+        })
+      );
+      const response = await controller.refresh({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {
+          [config.JWT.REFRESH_TOKEN_COOKIE_KEY]: random.alphaNumeric()
+        },
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+      assert(response.data && response.data.accessToken === accessToken);
+      stub.restore();
     });
   });
 
   describe('logout', () => {
-    it('calls logout on auth service', () => {
-      throw new Error('Not implemented yet');
+    it('calls logout on auth service', async () => {
+      const stub = sinon.stub(authService, 'logout');
+
+      await controller.logout({
+        body: {},
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+
+      assert(stub.calledOnce);
     });
   });
 
   describe('changePassword', () => {
-    it('calls changePassword on auth service', () => {
-      throw new Error('Not implemented yet');
+    it('calls changePassword on auth service', async () => {
+      const stub = sinon.stub(authService, 'changePassword');
+
+      await controller.changePassword({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+
+      assert(stub.calledOnce);
     });
   });
 
   describe('forgotPassword', () => {
-    it('calls forgotPassword on auth service', () => {
-      throw new Error('Not implemented yet');
+    it('calls forgotPassword on auth service', async () => {
+      const stub = sinon.stub(authService, 'forgotPassword');
+
+      await controller.forgotPassword({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+
+      assert(stub.calledOnce);
     });
   });
 
   describe('forgotPasswordSubmit', () => {
-    it('calls forgotPasswordSubmit on auth service', () => {
-      throw new Error('Not implemented yet');
+    it('calls forgotPasswordSubmit on auth service', async () => {
+      const stub = sinon.stub(authService, 'forgotPasswordSubmit');
+
+      await controller.forgotPasswordSubmit({
+        body: {
+          email: internet.email(),
+          password: internet.password()
+        },
+        cookies: {},
+        ip: internet.ip(),
+        params: {},
+        query: {}
+      });
+
+      assert(stub.calledOnce);
     });
   });
 });
--- test/middlewares/auth.spec.ts
+++ test/middlewares/auth.spec.ts
@@ -1,10 +1,53 @@
+import { assert } from 'chai';
+import { Request } from 'express';
+import { Response } from 'express-serve-static-core';
+import sinon from 'sinon';
+
+import { authMiddleware } from '../../src/middlewares/auth.middleware';
+
 describe('authMiddleware', () => {
   it('calls next if url is in ignored routes', () => {
-    throw new Error('Not implemented yet');
+    const ignoredUrls = [
+      '/auth/login',
+      '/auth/register',
+      '/auth/refresh',
+      '/auth/forgot-password',
+      '/auth/forgot-password-submit',
+      '/google/login'
+    ];
+
+    for (const url of ignoredUrls) {
+      const nextStub = sinon.stub();
+      authMiddleware(
+        {
+          url
+        } as Request,
+        {} as Response,
+        nextStub
+      );
+      assert(nextStub.called);
+    }
   });
 
   it('returns response with error if authorization header is missing', () => {
-    throw new Error('Not implemented yet');
+    const statusStub = sinon.stub();
+    const res: Response = ({
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        return this;
+      }
+    } as unknown) as Response;
+    authMiddleware(
+      {
+        headers: {}
+      } as Request,
+      res,
+      sinon.stub()
+    );
+    assert(statusStub.calledWith(401));
   });
 
   it('returns response with error if authorization header format is invalid', () => {
