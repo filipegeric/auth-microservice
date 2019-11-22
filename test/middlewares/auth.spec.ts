f5319f2be2613e2b64f0bf5b37bdbda073786972
Filip Egeric <filip.egeric@gmail.com>
2019-11-22 07:22:34 +0100

    Adds auth and rate limit middleware tests

--- test/middlewares/auth.spec.ts
+++ test/middlewares/auth.spec.ts
@@ -1,8 +1,11 @@
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { Request } from 'express';
 import { Response } from 'express-serve-static-core';
+import { internet } from 'faker';
+import { sign } from 'jsonwebtoken';
 import sinon from 'sinon';
 
+import { config } from '../../src/config';
 import { authMiddleware } from '../../src/middlewares/auth.middleware';
 
 describe('authMiddleware', () => {
@@ -31,12 +34,14 @@ describe('authMiddleware', () => {
 
   it('returns response with error if authorization header is missing', () => {
     const statusStub = sinon.stub();
+    const sendStub = sinon.stub();
     const res: Response = ({
       status(status: number) {
         statusStub(status);
         return this;
       },
       send() {
+        sendStub();
         return this;
       }
     } as unknown) as Response;
@@ -48,25 +53,142 @@ describe('authMiddleware', () => {
       sinon.stub()
     );
     assert(statusStub.calledWith(401));
+    assert(sendStub.called);
   });
 
   it('returns response with error if authorization header format is invalid', () => {
-    throw new Error('Not implemented yet');
+    const statusStub = sinon.stub();
+    const sendStub = sinon.stub();
+    const res: Response = ({
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        sendStub();
+        return this;
+      }
+    } as unknown) as Response;
+    authMiddleware(
+      {
+        headers: {
+          authorization: 'Bearertyuiop'
+        }
+      } as Request,
+      res,
+      sinon.stub()
+    );
+    assert(statusStub.calledWith(401));
+    assert(sendStub.called);
   });
 
-  it('returns response with error if token is invalid', () => {
-    throw new Error('Not implemented yet');
+  it('returns response with error if token is invalid', async () => {
+    const statusStub = sinon.stub();
+    const sendStub = sinon.stub();
+    const res: Response = ({
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        sendStub();
+        return this;
+      }
+    } as unknown) as Response;
+    await authMiddleware(
+      {
+        headers: {
+          authorization: 'Bearer qweqwe.asdasdasd.1312312'
+        }
+      } as Request,
+      res,
+      sinon.stub()
+    );
+    assert(statusStub.calledWith(401));
+    assert(sendStub.called);
   });
 
-  it('returns response with error if token payload is not an object', () => {
-    throw new Error('Not implemented yet');
+  it('returns response with error if token payload is not an object', async () => {
+    const statusStub = sinon.stub();
+    const sendStub = sinon.stub();
+    const res: Response = ({
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        sendStub();
+        return this;
+      }
+    } as unknown) as Response;
+    const token = sign('string payload', config.JWT.ACCESS_TOKEN_SECRET, {
+      algorithm: 'RS256'
+    });
+    await authMiddleware(
+      {
+        headers: {
+          authorization: `Bearer ${token}`
+        }
+      } as Request,
+      res,
+      sinon.stub()
+    );
+    assert(statusStub.calledWith(401));
+    assert(sendStub.called);
   });
 
-  it('returns response with error if there is no email in token payload', () => {
-    throw new Error('Not implemented yet');
+  it('returns response with error if there is no email in token payload', async () => {
+    const statusStub = sinon.stub();
+    const sendStub = sinon.stub();
+    const res: Response = ({
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        sendStub();
+        return this;
+      }
+    } as unknown) as Response;
+    const token = sign({ foo: 'bar' }, config.JWT.ACCESS_TOKEN_SECRET, {
+      algorithm: 'RS256'
+    });
+    await authMiddleware(
+      {
+        headers: {
+          authorization: `Bearer ${token}`
+        }
+      } as Request,
+      res,
+      sinon.stub()
+    );
+    assert(statusStub.calledWith(401));
+    assert(sendStub.called);
   });
 
-  it('attaches email to request', () => {
-    throw new Error('Not implemented yet');
+  it('attaches email to request', async () => {
+    const email = internet.email();
+    const token = sign({ email }, config.JWT.ACCESS_TOKEN_SECRET, {
+      algorithm: 'RS256'
+    });
+    const req: any = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    };
+    const res = {
+      status() {
+        return this;
+      },
+      send() {
+        return this;
+      }
+    };
+    await authMiddleware(
+      req as Request,
+      (res as unknown) as Response,
+      sinon.stub()
+    );
+    expect(req.email).to.be.eql(email);
   });
 });
--- test/middlewares/rate-limit.spec.ts
+++ test/middlewares/rate-limit.spec.ts
@@ -1,13 +1,126 @@
+import { assert } from 'chai';
+import { Request, Response } from 'express';
+import { internet } from 'faker';
+import { Redis } from 'ioredis';
+import sinon from 'sinon';
+
+import { config } from '../../src/config';
+import { makeRateLimitMiddleware } from '../../src/middlewares/rate-limit.middleware';
+import { CacheService } from '../../src/services/cache.service';
+
 describe('rateLimitMiddleware', () => {
-  it('increments request count on every request', () => {
-    throw new Error('Not implemented yet');
+  let mockCache: { [key: string]: any };
+  const timeouts: NodeJS.Timer[] = [];
+  const cacheService: CacheService = new CacheService({
+    incr(key: string) {
+      if (!mockCache[key]) {
+        mockCache[key] = 0;
+      }
+      mockCache[key]++;
+      return mockCache[key];
+    },
+    expire(key: string, seconds: number) {
+      if (!mockCache[key]) {
+        return;
+      }
+      timeouts.push(
+        setTimeout(() => {
+          delete mockCache[key];
+        }, seconds * 1000)
+      );
+    }
+  } as Redis);
+  const middleware = makeRateLimitMiddleware(
+    cacheService,
+    config.RATE_LIMIT.COUNT,
+    config.RATE_LIMIT.WINDOW_IN_SECONDS
+  );
+
+  beforeEach(() => {
+    mockCache = {};
   });
 
-  it('returns 429 if limit is exceeded', () => {
-    throw new Error('Not implemented yet');
+  afterEach(() => {
+    for (const timeout of timeouts) {
+      clearTimeout(timeout);
+    }
   });
 
-  it('calls next if request count is not greater than limit', () => {
-    throw new Error('Not implemented yet');
+  it('increments request count on every request', async () => {
+    const ip = internet.ip();
+    const req = {
+      ip
+    };
+    const res = {
+      status() {
+        return this;
+      },
+      send() {
+        return this;
+      }
+    };
+    for (let i = 0; i < 10; i++) {
+      await middleware(
+        req as Request,
+        (res as unknown) as Response,
+        sinon.stub()
+      );
+      assert(mockCache[`rate-limit:${ip}`] === i + 1);
+    }
+  });
+
+  it('returns 429 if limit is exceeded', async () => {
+    const ip = internet.ip();
+    const req = {
+      ip
+    };
+    const statusStub = sinon.stub();
+    const res = {
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        return this;
+      }
+    };
+
+    while (!statusStub.calledWith(429)) {
+      await middleware(
+        req as Request,
+        (res as unknown) as Response,
+        sinon.stub()
+      );
+    }
+
+    assert(statusStub.calledOnceWith(429));
+  });
+
+  it('calls next if request count is not greater than limit', async () => {
+    const ip = internet.ip();
+    const req = {
+      ip
+    };
+    const statusStub = sinon.stub();
+    const res = {
+      status(status: number) {
+        statusStub(status);
+        return this;
+      },
+      send() {
+        return this;
+      }
+    };
+
+    while (true) {
+      const nextStub = sinon.stub();
+      await middleware(req as Request, (res as unknown) as Response, nextStub);
+      if (statusStub.calledWith(429)) {
+        break;
+      }
+      assert(nextStub.called);
+    }
+
+    assert(statusStub.calledOnceWith(429));
   });
 });
